b556334ec6acaa0b523e6fa1408f44852fff200c
priscila2294 <priscilla_2294@hotmail.com>
2020-11-09 07:26:36 -0800

    I did my best

--- code-challenges.js
+++ code-challenges.js
@@ -9,38 +9,65 @@
 // --------------------1) Create a function that determines if a temperature is below boiling point, at boiling point or above boiling point. Boiling point is 212 degrees Farhenheit.
 // Use the test variables provided below. Expected outcome: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-var temp1 = 35
-var temp2 = 350
-var temp3 = 212
+// var temp1 = 35
+// var temp2 = 350
+// var temp3 = 212
 
+// const boiling = (boilPoint) => {
+//     if(boilPoint < 212){
+//       return `${boilPoint} is below boiling point`
+//     } else if(boilPoint > 212) {
+//       return `${boilPoint} is above boiling point`
+//     } else if(boilPoint === 212) {
+//         return `${boilPoint} is at boiling point`
+//     } else {
+//     return "something went wrong"
+// }
+// }
 
-
+// console.log(boiling(temp1));
+// console.log(boiling(temp2));
+// console.log(boiling(temp3))
 
 
 // --------------------2) Create a function that multiplies each number in the array by 5 using a for loop.
 // Use the test variable provided below. Expected outcome: [15, 35, 0, 30, -45]
 
-var myNumbers1 = [3, 7, 0, 6, -9]
+// var myNumbers1 = [3, 7, 0, 6, -9]
 
-
-
-
+//     for (let i=0; i<myNumbers1.length; i++) {
+//         console.log(myNumbers1[i] * 5)
+//     }
+// I think I am missing a couple things here. I was able to write this funcion down by looking at our notes in the syllabus. 
 
 // --------------------3) Create a function that multiplies each number in the array by 5 using map.
 // Use the test variable provided below. Expected outcome: [40, -35, 0, 30, 10]
 
-var myNumbers2 = [8, -7, 0, 6, 2]
+// var myNumbers2 = [8, -7, 0, 6, 2]
 
+// const multiplier = myNumbers2.map(value => {
+//     return value * 5
+// })
 
+// console.log(multiplier)
+
+//Same here. I think I am missing something again. 
 
 
 
 // --------------------4) Create a function that removes all the vowels from a string.
 // Use the test variables provided below. Expected output: "HyThrLrnStdnt" "LvJvScrpt"
 
-var stringWithVowels1 = "HeyThereLearnStudent"
-var stringWithVowels2 = "ILoveJavaScript"
+// var stringWithVowels1 = "HeyThereLearnStudent"
+// var stringWithVowels2 = "ILoveJavaScript"
+// let vowels [a, e, i, o, u]
 
+// const vowelRemover = (string) => {
+//       string = string.split ("")
+
+// }
+
+//I am so lost! I did try asking my classmates without asking for an answer, but I don't think that help a lot.
 
 
 
@@ -48,21 +75,64 @@ var stringWithVowels2 = "ILoveJavaScript"
 // --------------------5) Copy/paste your code from #4. Refactor your code to include non-string edge cases. Inform your user that the variable passed into the vowel removal function is not a string.
 // Use the test variables provided below. Expected output: "true is not a string" "42 is not a string"
 
-var notAString1 = true
-var notAString2 = 42
+// var notAString1 = true
+// var notAString2 = 42
+
+// var stringWithVowels1 = "HeyThereLearnStudent"
+// var stringWithVowels2 = "ILoveJavaScript"
+// let vowels [a, e, i, o, u]
+
+//      const vowelRemover = (string) =>
 
 
+// More lost than before
 
 
 
 // --------------------6) Create a function that takes the toonimals array and returns an array with only the toon objects that are cats.
 // Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
 
-var toonimals = [ { name: "Itchy", animal: "mouse" }, { name: "Stimpy", animal: "cat" }, { name: "Daffy", animal: "duck" }, { name: "Scratchy", animal: "cat" }, { name: "Ren", animal: "dog" }, { name: "Felix", animal: "cat" }]
+// var toonAnimals = [ { name: "Itchy", animal: "mouse" }, 
+//          { name: "Stimpy", animal: "cat" }, 
+//          { name: "Daffy", animal: "duck" }, 
+//          { name: "Scratchy", animal: "cat" }, 
+//          { name: "Ren", animal: "dog" }, 
+//          { name: "Felix", animal: "cat" }]
+
+// var catsOnly = []
+
+// const findToonCats = (array) => {
+//     catsOnly= array.filter(value => {
+//         return value.animal == "cat"
+//     })
+//     return catsOnly
+// }
+
+// console.log(findToonCats(toonAnimals));
 
 
-
-
+// Even though I had a lot of help for this exercise, I thought it was easier than #4 and #5. 
+// Am I missing something?
 
 // --------------------7) Using the toonimals variable from #6, create a function that returns only the names of the non-cats.
 // Expected output: "Itchy" "Daffy" "Ren"
+
+// var toonAnimals = [ { name: "Itchy", animal: "mouse" }, 
+//          { name: "Stimpy", animal: "cat" }, 
+//          { name: "Daffy", animal: "duck" }, 
+//          { name: "Scratchy", animal: "cat" }, 
+//          { name: "Ren", animal: "dog" }, 
+//          { name: "Felix", animal: "cat" }]
+
+// var catsOnly = []
+
+// const findToonCats = (array) => {
+//     catsOnly= array.filter(value => {
+//         return value.animal != "cat"
+//      })
+
+// }
+
+// console.log(findToonCats(toonAnimals))
+
+// What am I missing?
--- testing-with-jest.test.js
+++ testing-with-jest.test.js
@@ -8,20 +8,42 @@
 
 // --------------------1a) Create a test for a function called addThemUp that takes two numbers as arguments and returns the sum.
 
+// describe("addTwoNumbers", () =>{
+//     test("should add two arguments and return the sum", () => {
+//         expect(addTwoNumbers(9,10)).toEqual(19)
 
+//     })
+// })
+
+
+// When I try to run "yarn jest" on my terminal, this message appears : error Couldn't find a package.json file in "/Users/learnacademy/Desktop/week-1-assessment-priscila2294"
+info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command. 
 
 
 
 // --------------------1b) See the test fail. THEN write the code to make the test pass.
 
-
+// const addTwoNumbers = (num1, num2) => {
+//     return num1 + num2 
+// }
 
 
 
 // --------------------2a) Create a test for a function called triangleArea that takes the base and the height of a triange and returns the area. The area of a triange is the base times the height divided by two.
+
+// describe("trigArea",() => {
+
+//     test("calculate the area of a triangle by mutiplying the base and the height of it, and then divide it by two" , () => {
+//          expect(trigArea(5,6)).toEqual(15)     
+// })
+// })
 
 
 
 
 
 // --------------------2b) See the test fail. THEN write the code to make the test pass.
+
+//  const trigArea = (num1, num2) => {
+//      return (num1*num2)/2
+//  }
